480ed48b934d42392f40375fbb06689cc15f69ae
Daniel Zamarripa Bringas <dzamarripab@Daniels-MacBook-Air.local>
2023-09-27 15:39:15 -0600

    fix(server): fixed server start script

--- server/database.sql
+++ server/database.sql
@@ -1,24 +1,31 @@
 -- Create the database (if it doesn't exist)
 CREATE DATABASE resistor_calculator;
 
--- Create a schema for the database
-CREATE SCHEMA resistor_code;
+-- Create a user for the database
+CREATE USER resistor_user WITH PASSWORD 'your_secure_password';
+GRANT ALL PRIVILEGES ON DATABASE resistor_calculator TO resistor_user;
+GRANT ALL PRIVILEGES ON SCHEMA public TO resistor_user;
 
 -- Create a table for resistor colors
 CREATE TABLE resistor_colors (
-  id SERIAL PRIMARY KEY,
-  color VARCHAR(255) NOT NULL,
-  value INT NOT NULL,
-  multiplier INT NOT NULL,
-  tolerance INT NOT NULL
+    id SERIAL PRIMARY KEY,
+    color VARCHAR(255) NOT NULL,
+    value INT,
+    multiplier REAL,
+    tolerance REAL
 );
 
 -- Insert color values into the table
-INSERT INTO resistor_colors (color, value, multiplier, tolerance)
-VALUES
-  ('black', 0, 1, 20),
-  ('brown', 1, 10, 1),
-  ('red', 2, 100, 2),
-  ('orange', 3, 1000, 3),
-  ('yellow', 4, 10000, 4),
-  ('green', 5, 100000, 0);
+INSERT INTO resistor_colors (color, value, multiplier, tolerance) VALUES
+('Black', 0, 1, NULL),
+('Brown', 1, 10, 1),
+('Red', 2, 100, 2),
+('Orange', 3, 1000, NULL),
+('Yellow', 4, 10000, NULL),
+('Green', 5, 100000, 0.5),
+('Blue', 6, 1000000, 0.25),
+('Violet', 7, 10000000, 0.1),
+('Gray', 8, 100000000, 0.05),
+('White', 9, 1000000000, NULL),
+('Gold', NULL, 0.1, 5),
+('Silver', NULL, 0.01, 10);
